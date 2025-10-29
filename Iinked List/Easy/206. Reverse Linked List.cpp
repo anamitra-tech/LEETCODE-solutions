606838bba3e6edf3ee8a2125d0e15670f38586a1
Anamitra Bakshi <anamitra1205@gmail.com>
2025-10-29 14:40:52 +0530

    Create 206. Reverse Linked List.cpp

---
+++ Iinked List/Easy/206. Reverse Linked List.cpp
@@ -0,0 +1,11 @@
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        while (head) {
+            ListNode* nxt = head->next;
+            head->next = prev;
+            prev = head;
+            head = nxt;
+        }
+        return prev;
+    }
+};
