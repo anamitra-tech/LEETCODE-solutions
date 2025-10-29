ad14b68e6ee77a3ce080fc5632c684c4f58b1c1a
Anamitra Bakshi <anamitra1205@gmail.com>
2025-10-30 03:22:19 +0530

    Create 92.Reverse Linked List II.cpp

---
+++ Iinked List/Medium/92.Reverse Linked List II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if (!head || left == right) return head;
+
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prev = &dummy;
+
+        // Move prev to the node just before 'left'
+        for (int i = 1; i < left; i++)
+            prev = prev->next;
+
+        ListNode* curr = prev->next;
+        ListNode* next = nullptr;
+
+        // Reverse nodes between left and right
+        for (int i = 0; i < right - left; i++) {
+            next = curr->next;
+            curr->next = next->next;
+            next->next = prev->next;
+            prev->next = next;
+        }
+
+        return dummy.next;
+    }
+};
