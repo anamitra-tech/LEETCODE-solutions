7f09dd1eb17ba64e71178cc50db54dd3cb337816
Anamitra Bakshi <anamitra1205@gmail.com>
2025-10-30 14:23:51 +0530

    Create 901. Online Stock Span.cpp

---
+++ Stacks/Medium/901. Online Stock Span.cpp
@@ -0,0 +1,16 @@
+class StockSpanner {
+public:
+    stack<pair<int, int>> stk; // (price, span)
+
+    StockSpanner() {}
+
+    int next(int price) {
+        int span = 1;
+        while (!stk.empty() && stk.top().first <= price) {
+            span += stk.top().second;
+            stk.pop();
+        }
+        stk.push({price, span});
+        return span;
+    }
+};
