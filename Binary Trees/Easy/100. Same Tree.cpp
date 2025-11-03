8e0716c614a5cc544d8ef7f2a94790ab60c483a0
Anamitra Bakshi <anamitra1205@gmail.com>
2025-11-04 00:49:12 +0530

    Create 100. Same Tree.cpp

---
+++ Binary Trees/Easy/100. Same Tree.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return nlr(p, q);
+    }
+
+    bool nlr(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+        if (p->val != q->val) return false;
+
+        bool left = nlr(p->left, q->left);
+        bool right = nlr(p->right, q->right);
+
+        return left && right;
+    }
+};
