5be9284c9e04fff6e15e66e0221584e72768b374
Anamitra Bakshi <anamitra1205@gmail.com>
2025-11-04 00:59:46 +0530

    Create 239. Sliding Window Maximum.cpp

---
+++ Queues/Hard/239. Sliding Window Maximum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> result;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            while (!dq.empty() && nums[i] > dq.back())
+                dq.pop_back();
+
+            dq.push_back(nums[i]);
+
+            if (i >= k && dq.front() == nums[i - k])
+                dq.pop_front();
+
+            if (i >= k - 1)
+                result.push_back(dq.front());
+        }
+
+        return result;
+    }
+};
