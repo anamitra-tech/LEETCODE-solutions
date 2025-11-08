49ebc2037fe4d6aa43e8ccd201bd3571a732df85
Anamitra Bakshi <anamitra1205@gmail.com>
2025-11-08 13:56:59 +0530

    Create 543. Diameter of Binary Tree.cpp

---
+++ Binary Trees/Medium/543. Diameter of Binary Tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxDiameter = 0;
+
+    int dfs(TreeNode* root) {
+        if (!root) return 0;
+
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+
+        // Update max diameter at this node
+        maxDiameter = max(maxDiameter, left + right); // number of edges!
+
+        return 1 + max(left, right); // return depth
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        dfs(root);
+        return maxDiameter;
+    }
+};
