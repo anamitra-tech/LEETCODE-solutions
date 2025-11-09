f4e301dbe3032e64df1d1edb8d7718d27737587a
Anamitra Bakshi <anamitra1205@gmail.com>
2025-11-09 22:44:17 +0530

    Create 515. Find Largest Value in Each Tree Row.cpp

---
+++ Binary Trees/Medium/515. Find Largest Value in Each Tree Row.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        vector<int> result;
+        if (!root) return result;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int levelSize = q.size();
+            int maxVal = INT_MIN;  // Minimum possible to start
+
+            for (int i = 0; i < levelSize; ++i) {
+                TreeNode* temp = q.front();
+                q.pop();
+
+                maxVal = max(maxVal, temp->val);  // Track max at this level
+
+                if (temp->left)
+                    q.push(temp->left);
+
+                if (temp->right)
+                    q.push(temp->right);
+            }
+
+            result.push_back(maxVal);
+        }
+
+        return result;
+    }
+};
