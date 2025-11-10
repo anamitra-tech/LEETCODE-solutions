f8cd77ac8054a4d89aa9f71d62afadd02452d212
Anamitra Bakshi <anamitra1205@gmail.com>
2025-11-10 19:29:13 +0530

    Create 637. Average of Levels in Binary Tree.c

---
+++ Binary Trees/Medium/637. Average of Levels in Binary Tree.c
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> result;
+        if (!root) return result;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+            long long sum = 0;  // use long long to prevent overflow
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                sum += node->val;
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            double avg = (double)sum / size;
+            result.push_back(avg);
+        }
+
+        return result;
+    }
+};637
