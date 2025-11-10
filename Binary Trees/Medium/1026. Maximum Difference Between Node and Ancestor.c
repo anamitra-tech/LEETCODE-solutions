3f2aa72ccf8f664588094caf103824beea0b5c5a
Anamitra Bakshi <anamitra1205@gmail.com>
2025-11-10 20:15:19 +0530

    Create 1026. Maximum Difference Between Node and Ancestor.c

---
+++ Binary Trees/Medium/1026. Maximum Difference Between Node and Ancestor.c
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int dfs(TreeNode* root, int minVal, int maxVal) {
+        if (!root) return maxVal - minVal;
+
+        // Update min and max seen so far
+        minVal = min(minVal, root->val);
+        maxVal = max(maxVal, root->val);
+
+        int left = dfs(root->left, minVal, maxVal);
+        int right = dfs(root->right, minVal, maxVal);
+
+        return max(left, right);
+    }
+
+    int maxAncestorDiff(TreeNode* root) {
+        return dfs(root, root->val, root->val);
+    }
+};
