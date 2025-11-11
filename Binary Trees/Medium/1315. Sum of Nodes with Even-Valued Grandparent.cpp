340e27b3a95fac50f6857e70463c56435db910ce
Anamitra Bakshi <anamitra1205@gmail.com>
2025-11-11 20:29:39 +0530

    Create 1315. Sum of Nodes with Even-Valued Grandparent.cpp

---
+++ Binary Trees/Medium/1315. Sum of Nodes with Even-Valued Grandparent.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int sum = 0;
+
+    void dfs(TreeNode* node, TreeNode* parent, TreeNode* grandparent) {
+        if (!node) return;
+
+        if (grandparent && grandparent->val % 2 == 0) {
+            sum += node->val;
+        }
+
+        dfs(node->left, node, parent);
+        dfs(node->right, node, parent);
+    }
+
+    int sumEvenGrandparent(TreeNode* root) {
+        dfs(root, nullptr, nullptr);
+        return sum;
+    }
+};
