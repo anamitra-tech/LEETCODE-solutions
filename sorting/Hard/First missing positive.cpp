4b27b1a52c5727ce02590d033f3ef5c1c549e0c5
Anamitra Bakshi <anamitra1205@gmail.com>
2025-12-15 17:51:42 +0530

    Create First missing positive.cpp

---
+++ sorting/Hard/First missing positive.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            while (nums[i] >= 1 && nums[i] <= n &&
+                   nums[nums[i] - 1] != nums[i]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1)
+                return i + 1;
+        }
+
+        return n + 1;
+    }
+};
+
+//here the cyclic sort has been used and its the only sort whose time complexity is O(n)
