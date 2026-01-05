27b2af2d484c70004ad8078dbe75315083f0aa18
Anamitra Bakshi <anamitra1205@gmail.com>
2026-01-05 20:26:05 +0530

    Implement Decode Ways solution in C++

---
+++ Dynamic Programming/Medium/91. Decode Ways.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        if (n == 0 || s[0] == '0') return 0;
+
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;   // ground
+        dp[1] = 1;   // first step (already checked != '0')
+
+        for (int i = 2; i <= n; i++) {
+
+            // 1-step move
+            if (s[i - 1] != '0') {
+                dp[i] += dp[i - 1];
+            }
+
+            // 2-step move
+            int two = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
+            if (two >= 10 && two <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+};
