e49d4202cdb91052c895e9de5adcf2d868a80292
Anamitra Bakshi <anamitra1205@gmail.com>
2026-01-18 14:08:07 +0530

    Update 199. Binary Tree Right Side View.cpp

--- Binary Trees/Medium/199. Binary Tree Right Side View.cpp
+++ Binary Trees/Medium/199. Binary Tree Right Side View.cpp
@@ -5,33 +5,25 @@ public:
         if (!root) return result;
 
         queue<TreeNode*> q;
-        stack<TreeNode*> st;
-
         q.push(root);
 
         while (!q.empty()) {
             int size = q.size();
+            TreeNode* node = nullptr;
 
-            // Push all nodes of the current level into the stack
             for (int i = 0; i < size; i++) {
-                TreeNode* node = q.front();
+                node = q.front();
                 q.pop();
-                st.push(node);
 
-                // Add children for the next level
+                // Push children in the queue
                 if (node->left)
                     q.push(node->left);
                 if (node->right)
                     q.push(node->right);
             }
 
-            // The top of the stack is the rightmost node of the level
-            TreeNode* temp = st.top();
-            result.push_back(temp->val);
-
-            // Clear the stack for next level
-            while (!st.empty())
-                st.pop();
+            // After finishing the level, 'node' is the last node (rightmost)
+            result.push_back(node->val);
         }
 
         return result;
